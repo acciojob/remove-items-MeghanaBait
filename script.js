e53b189ea71d3aa4d10d0607ae927685ddad543a
MeghanaBait <133511757+MeghanaBait@users.noreply.github.com>
2023-12-29 17:00:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+    let selectElement = document.getElementById('colorSelect');
+    let removeButton = document.querySelector('input[type="button"]');
+    
+    removeButton.addEventListener('click', function() {
+        let selectedOption = selectElement.options[selectElement.selectedIndex];
+        if (selectedOption) {
+            selectElement.removeChild(selectedOption);
+        }
+    });
+});
